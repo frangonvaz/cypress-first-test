c6898e4fae167b8ed293b3441223d3882fef6f7a
frangonvaz <pacodaw777@gmail.com>
2021-06-17 23:38:11 +0200

    First commit :)

---
+++ cypress/integration/examples/primera-prueba.js
@@ -0,0 +1,19 @@
+//Suite de casos que contiene cada caso
+describe('Primer conjunto casos de prueba', () => {
+
+    //Caso 1
+    it('Ingresar a la pag principal', () => {
+        //Paso 1
+        //Paso 2
+        //Paso 3
+
+        cy.visit('http://automationpractice.com/index.php');
+        cy.get('.login').click();
+
+    });
+    //Caso 2
+
+    //Caso 3
+
+
+})
